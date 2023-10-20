d1d2a008ba977b778a136afd1c7153f4ee13fb11
dev <113306065+dev4lov@users.noreply.github.com>
2023-10-20 21:01:29 +0530

    Add files via upload

--- whatsapp-frontend/src/Chat.jsx
+++ whatsapp-frontend/src/Chat.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import "./Chat.css";
 import { Avatar, IconButton } from "@mui/material";
 import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
@@ -6,22 +5,41 @@ import AttachFileIcon from "@mui/icons-material/AttachFile";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import InsertEmoticonIcon from "@mui/icons-material/InsertEmoticon";
 import MicNoneIcon from "@mui/icons-material/MicNone";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import axios from "./axios";
 
-export default function Chat({ messages }) {
-  const sendMessage = async (e) => {
-    e.preventDefault();
+export default function Chat() {
+  const messagesEndRef = useRef(null);
 
-    await axios.post("/messages/new", {
-      message: input,
-      name: "Dev",
-      received: "false",
-    });
-    setInput("");
+  const scrollToBottom = () => {
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
   };
 
   const [input, setInput] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("messages/sync")
+      .then((response) => setMessages(response.data))
+      .then(() => scrollToBottom());
+  }, []);
+
+  const sendMessage = async (e) => {
+    e.preventDefault();
+    const msg = input;
+
+    if (!msg) return;
+    setInput("");
+
+    const newMessage = { message: msg, name: "Beluga", received: false, timestamp: new Date().toLocaleString() };
+
+    messages.push(newMessage);
+    scrollToBottom();
+    
+    await axios.post("/messages/new", newMessage);
+  };
+
   return (
     <div className="chat">
       <div className="chat__header">
@@ -46,25 +64,21 @@ export default function Chat({ messages }) {
 
       <div className="chat__body">
         {messages.map((message) => (
-          <p
-            className={`chat__message ${!message.received && "chat__reciever"}`}
-          >
-            <span className="chat__name">{message.name}</span>
-            {message.message}
-            <span className="chat__timestamp">{message.timestamp}</span>
-          </p>
+          <div key={message.timestamp}>
+            <p className={`chat__message ${!message.received && "chat__reciever"}`}>
+              <span className="chat__name">{message.name}</span>
+              {message.message}
+              <span className="chat__timestamp">{message.timestamp}</span>
+            </p>
+          </div>
         ))}
+        <div className="scroll__end" ref={messagesEndRef} />
       </div>
 
       <div className="chat__footer">
         <InsertEmoticonIcon />
         <form action="">
-          <input
-            value={input}
-            onChange={(e) => setInput(e.target.value)}
-            type="text"
-            placeholder="Type a message"
-          />
+          <input value={input} onChange={(e) => setInput(e.target.value)} type="text" placeholder="Type a message" />
           <button onClick={sendMessage} type="submit">
             Submit
           </button>
